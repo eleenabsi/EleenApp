c5d6e139862b548aebd406854999842f43c5b346
eleenabsi <eleen1absi@gmail.com>
2023-04-19 00:53:57 +0300

    first

---
+++ app/src/main/java/com/example/eleenapp/CatActvity.java
@@ -0,0 +1,14 @@
+package com.example.eleenapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CatActvity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat_actvity);
+    }
+}
